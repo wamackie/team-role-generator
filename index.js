47f8adfb5c429caa99d6705af3dca85a53e465ce
Wamackie <wamackie8456@gmail.com>
2021-08-27 00:35:33 -0400

    updated index

---
+++ index.js
@@ -0,0 +1,31 @@
+const fs = require("fs");
+const inquirer = require("inquirer");
+
+const Employee = require("./lib/Employee");
+const Manager = require("./lib/Manager");
+const Engineer = require("./lib/Engineer");
+const Intern = require("./lib/Intern");
+
+const createTeam = () => inquirer.prompt([
+    {
+        type: "input",
+        name: "name",
+        message: "What is the manager's name?"
+    },
+    {
+        type: "input",
+        name: "id",
+        message: "What is the manager's ID number?"
+    },
+    {
+        type: "input",
+        name: "email",
+        message: "What is the manager's email?"
+    },
+    {
+        type: "input",
+        name: "officeNumber",
+        message: "What is the manager's office number?"
+    }
+])
+
